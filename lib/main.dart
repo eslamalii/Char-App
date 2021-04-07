5fdde7f216ca040ee825f9f2cb6c81b093793267
Eslam Ali <preslamali@hotmail.com>
2021-04-07 08:16:07 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:chart_app/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +21,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(id: '1', title: 'Test', amount: 99.99, date: DateTime.now()),
+    Transaction(id: '2', title: 'Test21', amount: 992.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,15 +33,45 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Column(
-          children: <Widget>[
-            Card(
-              child: Container(
-                child: Center(child: Text('Hello')),
-                width: double.infinity,
+          children: transaction.map((tx) {
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                          fontSize: 13),
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Spacer(),
+                  Column(
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(color: Colors.black26),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                ],
               ),
-              elevation: 5,
-            )
-          ],
+            );
+          }).toList(),
         ));
   }
 }
